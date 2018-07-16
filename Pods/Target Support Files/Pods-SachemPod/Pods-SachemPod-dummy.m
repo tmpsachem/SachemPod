6bd86bb41c03c71c0631a5c6c24ab14bdab5a23e
Mark Yankovsky <sachem@Marks-MacBook-Pro.local>
2018-07-16 16:07:59 +0300

    Initial commit

---
+++ Pods/Target Support Files/Pods-SachemPod/Pods-SachemPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SachemPod : NSObject
+@end
+@implementation PodsDummy_Pods_SachemPod
+@end
---
+++ SachemPod/SPShortcuts.swift
@@ -0,0 +1,16 @@
+//
+//  SPShortcuts.swift
+//  SachemPod
+//
+//  Created by Mark Yankovsky on 7/16/18.
+//  Copyright © 2018 Mark Yankovsky. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+typealias SKSN = SKSpriteNode
+typealias UIC = UIColor
+typealias CGF = CGFloat
+typealias UIVC = UIViewController
