d705c1aba3be60aa86025cc91a3f8e8e030564b6
Mark Yankovskiy <sachem@marks-macbook-pro.local>
2018-07-16 15:26:38 +0300

    Initial Commit

---
+++ SachemPod/SachemPod.h
@@ -0,0 +1,19 @@
+//
+//  SachemPod.h
+//  SachemPod
+//
+//  Created by Mark Yankovsky on 7/16/18.
+//  Copyright © 2018 Mark Yankovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SachemPod.
+FOUNDATION_EXPORT double SachemPodVersionNumber;
+
+//! Project version string for SachemPod.
+FOUNDATION_EXPORT const unsigned char SachemPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SachemPod/PublicHeader.h>
+
+
